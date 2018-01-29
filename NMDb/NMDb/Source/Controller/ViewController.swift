9c6f3ab927283cedb13377e0cdbcad4eb40372c2
Gian Nucci <gianln@ciandt.com>
2018-01-29 14:47:59 -0200

    New project + setup

---
+++ NMDb/NMDb/Source/Controller/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NMDb
+//
+//  Created by Gian Nucci on 29/01/18.
+//  Copyright © 2018 nucci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func parse(_ sender: Any) {
+        
+    }
+}
